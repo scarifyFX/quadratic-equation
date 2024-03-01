a086183ab511bc146b5cbee841042e3c93cfc59c
cyde <47931007+scarifyFX@users.noreply.github.com>
2024-03-01 20:49:05 +0100

    Update andragradsekvation.py

--- andragradsekvation.py
+++ andragradsekvation.py
@@ -1,30 +1,28 @@
-def create_quadratic_equation(x1, x2, y):
+def create_quad_equa(x1, x2, y):
     if y == 0:
-        # If y-intercept is 0, it means one of the roots is also 0
-        # In this case, we can factor out x from one of the roots to get the quadratic equation
         if x1 == 0:
-            equation = f"x(x - {x2}) = 0"
+            equation = f"x(x - {x2}) = y"
         elif x2 == 0:
-            equation = f"x(x - {x1}) = 0"
+            equation = f"x(x - {x1}) = y"
         else:
-            equation = f"(x - {x1})(x - {x2}) = 0"
+            equation = f"(x - {x1})(x - {x2}) = y"
     else:
         a = y / (x1 * x2)
         b = -a * (x1 + x2)
-        c = a * x1 * x2
-        equation = f"{a}x^2 + {b}x + {c} = 0"
+        c = a * x1 * x1
+        equation = f"{a}x^2 + {b}x + {c} = y"
 
     return equation
 
 def main():
-    x1 = float(input("Enter the first zero point (x1): "))
-    x2 = float(input("Enter the second zero point (x2): "))
-    y = float(input("Enter the y-intercept: "))
-
-    quadratic_equation = create_quadratic_equation(x1, x2, y)
-    print("The quadratic equation is:", quadratic_equation)
+    x1 = float(input("Enter the first Zero Point (X1): "))
+    x2 = float(input("Enter the first Zero Point (X2): "))
+    y = float(input("Enter the Y-intercept (y): "))
+    
+    quad_equa = create_quad_equa(x1, x2, y)
+    print("The quadratic equation is:", quad_equa)
 
 if __name__ == "__main__":
-    main()
+  main()
 
 confirmation = input("Press ENTER to exit")
